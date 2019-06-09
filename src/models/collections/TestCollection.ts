a370b9610267a09fbc679df2992777a970732418
Lars Johan <lars.johan94@gmail.com>
2019-06-09 16:48:16 +0200

    Removed test-collection

--- src/models/collections/TestCollection.ts
+++
@@ -1,9 +0,0 @@
-import CollectionBase from '../infrastructure/CollectionBase';
-import TestEntity from '../TestEntity';
-
-export default class TestCollection extends CollectionBase<TestEntity> {
-
-    constructor() {
-        super('test');
-    }
-}
--- src/plugins/DataAccessPlugin.ts
+++ src/plugins/DataAccessPlugin.ts
@@ -1,12 +1,8 @@
-import * as tedb from 'tedb';
-import * as storageDriver from 'tedb-electron-storage';
-
 
 export default class DataAccessPlugin {
-    public readonly testRepo: TestCollection;  
     
     constructor() {
-        this.testRepo = new TestCollection();
+        
     }
 
     install(vue: any, options: any) {
@@ -17,7 +13,6 @@ export default class DataAccessPlugin {
 
 // Declare custom types on the Vue-instance
 import Vue from 'vue';
-import TestCollection from '@/models/collections/TestCollection';
 
 declare module 'vue/types/vue' {
     
